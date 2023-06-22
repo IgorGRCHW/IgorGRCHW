dd302f90762dce8499c1852caf17ebff65742dae
IgorGRCHW <kacperskiigor@gmail.com>
2023-06-22 13:40:58 +0200

    Dodanie plikow

---
+++ nie/Main1.java.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
